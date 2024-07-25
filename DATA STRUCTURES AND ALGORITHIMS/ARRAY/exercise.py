2aeb6ee797f8ca8fc46a8e56115f94cb2711113e
JeffLiw <jeffreyliwus@gmail.com>
2024-07-24 21:01:45 -0400

    Add files via upload

---
+++ DATA STRUCTURES AND ALGORITHIMS/ARRAY/exercise.py
@@ -0,0 +1,19 @@
+var = None
+print(var)
+mylist = [1, 2, 23]
+mylist2 = [1, 2, 23]
+mylist[-1]
+print(mylist[-1])
+
+if mylist not in mylist2:
+    print("a is not in b")
+print(mylist.count(1))
+print(mylist)
+print(len(mylist))
+mylist.pop()
+print(mylist)
+mylist.sort()
+print(mylist)
+mylist.reverse()
+print(mylist)
+mylist.count()
